2f28d113d6bbe840c9f39e2f0b0384ca7b3171e9
HTXuom <xuom.ho25@student.passerellesnumeriques.org>
2024-01-09 21:37:17 +0700

    commit file

--- views/web/partials/header.view.php
+++ views/web/partials/header.view.php
@@ -10,10 +10,8 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <?php include "./assets/web/css/auth.css.php" ?>
     <!-- link header and footer -->
     <?php include("./assets/web/css/header.footer.css.php") ?>
-
     <?php include("./assets/web/css/home.css.php") ?>
     <?php include("./assets/web/css/about.css.php") ?>
     <?php include("./assets/web/css/room.css.php") ?>
